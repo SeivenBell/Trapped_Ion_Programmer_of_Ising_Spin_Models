7a1775a9f8b569efdc2aefb74ff3dea5f2b4fcde
Severyn "Seiven" Balaniuk <sbalaniu@uwaterloo.ca>
2023-11-23 15:44:22 -0500

    Model clarification

--- README.md
+++ README.md
@@ -2,4 +2,9 @@
 Replicating work on ion programming with additional code optimization
 
 
-Here we will talk about The Plan, flow of the project and responsibiliites and deadlines, I guess
+Here we will talk about The Plan, flow of the project and responsibiliites and deadlines, I guess.
+
+Our workflow: 
+1. Each block of the project will have their own branch to avoid conflicts and mess
+2. Branches will live for a day or two. People should push at least 2 changes to each branch to practice 
+3. 
--- triprism/model.py
+++ triprism/model.py
@@ -11,19 +11,35 @@ import trical
 
 class SpinDecoder(nn.Module):
     def __init__(self, ti, mu, deltak=torch.Tensor([1, 0, 0]) * 4 * torch.pi / 355e-9):
+        # Purpose: This class seems to decode a set of inputs 
+        # into a form that represents the interactions in a quantum system (likely ion spins).
+        # ti: An object representing the trapped ions in the quantum system.
+        # mu: A tensor representing frequencies of the quantum system.
+        # deltak: A default tensor value representing the change in wave vector (connection
+        #           between the wave vector and the frequency).
         
 
         pass
 
     def vectorize_J(self, x):
-        
+        # Converts a matrix of interactions (J) into a vector form. 
+        # This is useful for processing in neural networks.
+        idcs = torch.triu_indices(self.N, self.N, 1)
+        y = x[..., idcs[0], idcs[1]]
         return y
 
     def matrixify_J(self, x):
+        # The inverse of vectorize_J; it converts the vectorized interactions back into a matrix form.
         
         return J
 
     def forward(self, x):
+        # x: A tensor of interactions (J)
+        # y: A tensor of ion spins (S)
+        # computes the interaction matrix from the input 
+        # vectorizes it, normalizes it, and then returns the result.
+        # einsum("...im,in->...imn", x, self.eta)
+        # einsum("...imn,...jmn->...ij", y, y * self.w_mult_nu)
         
         return y
 
@@ -35,16 +51,23 @@ class SpinDecoder(nn.Module):
 
 class RabiEncoder(nn.Module):
     def __init__(self, N, N_h, activation=torch.nn.Identity()):
+        # N: The size of the input.
+        # N_h: The number of hidden units.
+        # activation: An activation function (default is identity).
         
+        # self.linear and self.linear2) for transforming the input data.
 
         pass
 
     def forward(self, x):
-       
+       # Processes the input through the linear layers and an activation function
+       # (ReLU followed by the specified activation function), and reshapes the output to 
+       # a specific format.
 
         return y
 
     def forwardOmega(self, x):
+       # pass that normalizes the output of the forward method.
         
         return y
 
@@ -52,15 +75,19 @@ class RabiEncoder(nn.Module):
 
 
 class kRankRabiEncoder(nn.Module):
+    #k: the rank for the approximation.
     def __init__(self, k, N, N_h, activation=torch.nn.Identity()):
         super().__init__()
+         #it adds another linear layer (self.linear3)
+        
 
         
 
         pass
 
     def forward(self, x):
-        
+        # Similar to RabiEncoder, but processes the input into two separate paths (y1 and y2) 
+        # 2 reshapes ruquired (y1 and y2)
         y = (y1, y2)
 
         return y
@@ -105,9 +132,12 @@ class PrISM(nn.Module):
         return 1 - torch.einsum("...i,...i->...", y, x).mean()
 
     def reconstruction_loss(self, x):
+        # einsum needed torch.einsum("...i,...i->...",
         
 
     def dynamic_range_penalty(self, x):
+        #Computes a penalty based on the dynamic range of the encoded output, 
+        # encouraging model stability.
     
         return (Omega.std() / Omega.mean()) ** 2
 
